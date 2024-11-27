da1f8d0314ad01a1a9254fa32d8bbb3efe2b6971
Micrad <leszekwita@o2.pl>
2024-11-27 19:11:02 +0100

    update App.js log

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,12 +23,6 @@ module.exports = {
     WorldChainSepoliaTestnet: {
     url: `https://worldchain-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
     accounts: privateKeys.split(",")
-  },localhost: {
-    url: "http://127.0.0.1:8545",
-    gas: "auto",
-    gasPrice: "auto",
-  },
-
-
+  }
   }
 };
